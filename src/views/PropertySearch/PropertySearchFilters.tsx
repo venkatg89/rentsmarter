110beeb72c20edf3d16dcaa69189571ede428fda
Venkatesh Grandhi <vgrandhi@bn.com>
2022-05-05 23:17:31 +0530

    Pushed the UI Fixes

--- src/views/PropertySearch/PropertySearchFilters.tsx
+++ src/views/PropertySearch/PropertySearchFilters.tsx
@@ -6,73 +6,80 @@ import {
   Image,
   ScrollView,
   StyleSheet,
+  Text,
 } from "react-native";
 import styles from "./PropertySearchStyles";
-import { ButtonGroup, Text, Input, Icon } from "react-native-elements";
+import { ButtonGroup, Input, Icon } from "react-native-elements";
 import { Picker } from "@react-native-picker/picker";
+import SelectDropdown from "react-native-select-dropdown";
 import { Slider } from "@miblanchard/react-native-slider";
+import Fonts from "../../Themes/Fonts";
+import { Divider } from "react-native-paper";
 
 type props = {};
 
 const bedRooms = ["Any", "Studio", "1", "2", "3", "4", "5+"];
 const bathRooms = ["Any", "1", "2", "3+"];
 const Item = Picker.Item;
-const Min_Monthly_Rent = 0;
-const Max_Monthly_Rent = 2000;
-const Default_Min_Monthly_Rent = 400;
-const Default_Max_Monthly_Rent = 800;
+
+const MIN = 500;
 
 const PropertySearchFilters: React.FC<props> = ({}) => {
-  let sliderRef: any = null;
-
   const [selectedBedRoom, setSelectedBedRoom] = useState([]);
   const [selectedBathRoom, setSelectedBathRoom] = useState([]);
-  const [selectedMinSquareFeet, setSelectedMinSquareFeet] = useState("900");
-  const [selectedMaxSquareFeet, setSelectedMaxSquareFeet] = useState("1100");
-  const [selectedMonthlyRent, setSelectedMonthlyRent] = useState([
-    Default_Min_Monthly_Rent,
-    Default_Max_Monthly_Rent,
-  ]);
-  const [minMonthlyRent, setMinMonthlyRent] = useState(
-    Default_Min_Monthly_Rent.toString()
-  );
-  const [maxMonthlyRent, setMaxMonthlyRent] = useState(
-    Default_Max_Monthly_Rent.toString()
-  );
+  const [selectedMinSquareFeet, setSelectedMinSquareFeet] = useState("500");
+  const [selectedMaxSquareFeet, setSelectedMaxSquareFeet] = useState("2000");
+  const [selectedMonthlyRent, setSelectedMonthlyRent] = useState([400, 800]);
+
+  const minAndMaxValues = () => {
+    return Array.from(Array(16).keys()).map((current, i) => {
+      return MIN + current * 100;
+    });
+  };
 
   return (
     <View style={{ flex: 1 }}>
       <SafeAreaView style={styles.mainContainer}>
         <View style={{ flex: 1 }}>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={[instyles.text, { flex: 0.5 }]} h4>
+          <View
+            style={{
+              flexDirection: "row",
+              marginHorizontal: 10,
+            }}
+          >
+            <Text
+              style={[
+                instyles.text,
+                {
+                  flex: 0.5,
+                  alignSelf: "center",
+                },
+              ]}
+            >
               Montly Rent
             </Text>
             <Input
-              keyboardType={"numeric"}
-              containerStyle={{
-                flex: 0.3,
-              }}
+              containerStyle={[{ flex: 0.3 }, instyles.textInputStyleClass]}
               placeholder="Min Rent"
               leftIcon={{ type: "font-awesome", name: "dollar", size: 20 }}
-              value={minMonthlyRent}
-              onChangeText={(text) => {
-                if (
-                  parseInt(text) >= Min_Monthly_Rent &&
-                  parseInt(text) <= Max_Monthly_Rent
-                ) {
-                  const item = [parseInt(text), parseInt(maxMonthlyRent)];
-                  console.log("item", item);
-                  setSelectedMonthlyRent(item);
-                }
-                setMinMonthlyRent(text);
+              onChangeText={(number) => {
+                setSelectedMonthlyRent((prevState) => [
+                  Number(number),
+                  prevState[1],
+                ]);
               }}
+              value={selectedMonthlyRent[0].toString()}
+              underlineColorAndroid="transparent"
+              textAlignVertical={"center"}
+              textAlign={"right"}
+              inputContainerStyle={[{ borderBottomWidth: 0 }]}
             />
             <Text
               style={[
                 {
                   flex: 0.1,
                   alignSelf: "center",
+                  textAlign: "center",
                 },
               ]}
               h4
@@ -80,21 +87,24 @@ const PropertySearchFilters: React.FC<props> = ({}) => {
               -
             </Text>
             <Input
-              containerStyle={{ flex: 0.3 }}
+              containerStyle={[{ flex: 0.3 }, instyles.textInputStyleClass]}
+              onChangeText={(number) => {
+                setSelectedMonthlyRent((prevState) => [
+                  prevState[0],
+                  Number(number),
+                ]);
+              }}
               placeholder="Max Rent"
               leftIcon={{ type: "font-awesome", name: "dollar", size: 20 }}
-              value={maxMonthlyRent}
-              onChangeText={(text) => {
-                if (
-                  parseInt(text) >= Min_Monthly_Rent &&
-                  parseInt(text) <= Max_Monthly_Rent
-                ) {
-                  const item = [parseInt(minMonthlyRent), parseInt(text)];
-                  console.log("item", item);
-                  setSelectedMonthlyRent(item);
-                }
-                setMaxMonthlyRent(text);
-              }}
+              value={
+                selectedMonthlyRent.length > 0
+                  ? `${selectedMonthlyRent[1]}`
+                  : ""
+              }
+              underlineColorAndroid="transparent"
+              textAlignVertical={"center"}
+              textAlign={"right"}
+              inputContainerStyle={[{ borderBottomWidth: 0 }]}
             />
           </View>
           {/* <SliderContainer
@@ -104,21 +114,24 @@ const PropertySearchFilters: React.FC<props> = ({}) => {
           <Slider
             containerStyle={{ marginHorizontal: 10 }}
             animateTransitions
-            maximumTrackTintColor="#d3d3d3"
-            maximumValue={Max_Monthly_Rent}
-            minimumTrackTintColor="#1fb28a"
-            minimumValue={Min_Monthly_Rent}
+            maximumValue={2000}
+            minimumValue={0}
             value={selectedMonthlyRent}
-            thumbTintColor="#1a9274"
-            step={1}
+            step={10}
+            thumbTintColor="#2089dc"
             onValueChange={(value) => {
-              setMinMonthlyRent(value[0].toString());
-              setMaxMonthlyRent(value[1].toString());
+              console.log(".....", value);
               setSelectedMonthlyRent(value);
             }}
           />
+          <Divider style={{ height: 3 }} />
           {/* </SliderContainer> */}
-          <Text style={instyles.text} h4>
+          <Text
+            style={[
+              instyles.text,
+              { marginHorizontal: 10, marginVertical: 10 },
+            ]}
+          >
             Bedrooms
           </Text>
           <ButtonGroup
@@ -140,7 +153,13 @@ const PropertySearchFilters: React.FC<props> = ({}) => {
             }}
             containerStyle={{ marginBottom: 20 }}
           />
-          <Text style={instyles.text} h4>
+          <Divider style={{ height: 3 }} />
+          <Text
+            style={[
+              instyles.text,
+              { marginHorizontal: 10, marginVertical: 10 },
+            ]}
+          >
             Bathrooms
           </Text>
           <ButtonGroup
@@ -162,44 +181,122 @@ const PropertySearchFilters: React.FC<props> = ({}) => {
             }}
             containerStyle={{ marginBottom: 20 }}
           />
-          <Text style={instyles.text} h4>
+          <Text
+            style={[
+              instyles.text,
+              { marginHorizontal: 10, marginVertical: 10 },
+            ]}
+          >
             Squarefeet
           </Text>
           <View
             style={{
               flexDirection: "row",
+              width: "80%",
+              justifyContent: "space-between",
+              padding: 10,
             }}
           >
-            <View style={{ flex: 0.5 }}>
+            <View style={{ width: "30%" }}>
               <Text
                 style={{ textAlign: "center", textAlignVertical: "center" }}
               >
                 {"Minimum"}
               </Text>
-              <Picker
+              {/* <Picker
                 selectedValue={selectedMinSquareFeet}
                 onValueChange={(v) => setSelectedMinSquareFeet(v)}
-                mode={"dropdown"}
+                mode={'dropdown'}
               >
-                <Item label="900" value="900" />
-                <Item label="1000" value="1000" />
-              </Picker>
+                {minAndMaxValues().map((value) => (
+                  <Item label={value.toString()} value={value} />
+                ))}
+              </Picker> */}
+              <View style={{ paddingVertical: 10 }}>
+                <SelectDropdown
+                  data={minAndMaxValues()}
+                  onSelect={(selectedItem, index) => {
+                    console.log(selectedItem, index);
+                    setSelectedMinSquareFeet(selectedItem);
+                  }}
+                  defaultButtonText={"Select"}
+                  buttonTextAfterSelection={(selectedItem, index) => {
+                    return selectedItem;
+                  }}
+                  rowTextForSelection={(item, index) => {
+                    return item;
+                  }}
+                  buttonStyle={{
+                    width: 160,
+                    height: 40,
+                    backgroundColor: "#FFF",
+                    borderRadius: 4,
+                    borderWidth: 1,
+                    borderColor: "#444",
+                  }}
+                  dropdownStyle={{ height: 200 }}
+                  renderDropdownIcon={() => {
+                    return (
+                      <Image
+                        source={require("../../assets/Images/expand.png")}
+                        style={{ width: 12, height: 12, marginRight: 10 }}
+                      />
+                    );
+                  }}
+                />
+              </View>
             </View>
-            <View style={{ flex: 0.5 }}>
+            {/* <View style={{ flex: 0.2 }} /> */}
+            <View style={{ width: "30%" }}>
               <Text
                 style={{ textAlign: "center", textAlignVertical: "center" }}
               >
                 {"Maximum"}
               </Text>
-              <Picker
+              {/* <Picker
                 selectedValue={selectedMaxSquareFeet}
                 onValueChange={(v) => setSelectedMaxSquareFeet(v)}
               >
-                <Item label="1100" value="1100" />
-                <Item label="1200" value="1200" />
-                <Item label="1240" value="1240" />
-                <Item label="1290" value="1290" />
-              </Picker>
+                {minAndMaxValues().map((value) => (
+                  <Item label={value.toString()} value={value} />
+                ))}
+              </Picker> */}
+              <View style={{ paddingVertical: 10 }}>
+                <SelectDropdown
+                  data={minAndMaxValues()}
+                  onSelect={(selectedItem, index) => {
+                    setSelectedMaxSquareFeet(selectedItem);
+                  }}
+                  defaultButtonText={"Select"}
+                  buttonTextAfterSelection={(selectedItem, index) => {
+                    // text represented after item is selected
+                    // if data array is an array of objects then return selectedItem.property to render after item is selected
+                    return selectedItem;
+                  }}
+                  rowTextForSelection={(item, index) => {
+                    // text represented for each item in dropdown
+                    // if data array is an array of objects then return item.property to represent item in dropdown
+                    return item;
+                  }}
+                  buttonStyle={{
+                    width: 160,
+                    height: 40,
+                    backgroundColor: "#FFF",
+                    borderRadius: 4,
+                    borderWidth: 1,
+                    borderColor: "#444",
+                  }}
+                  dropdownStyle={{ height: 200 }}
+                  renderDropdownIcon={() => {
+                    return (
+                      <Image
+                        source={require("../../assets/Images/expand.png")}
+                        style={{ width: 12, height: 12, marginRight: 10 }}
+                      />
+                    );
+                  }}
+                />
+              </View>
             </View>
           </View>
         </View>
@@ -210,8 +307,29 @@ const PropertySearchFilters: React.FC<props> = ({}) => {
 
 const instyles = StyleSheet.create({
   text: {
+    fontSize: Fonts.size.medium,
     textAlign: "left",
-    padding: 5,
+    textAlignVertical: "center",
+    fontWeight: "700",
+  },
+  textInputStyleClass: {
+    // Setting up Hint Align center.
+    textAlign: "right",
+
+    // Setting up TextInput height as 50 pixel.
+    height: 45,
+
+    // Set border width.
+    borderWidth: 0.5,
+
+    // Set border Hex Color Code Here.
+    borderColor: "gray",
+
+    // Set border Radius.
+    borderRadius: 5,
+
+    //Set background color of Text Input.
+    backgroundColor: "#FFFFFF",
   },
 });
 
